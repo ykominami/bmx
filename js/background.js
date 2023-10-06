e852524a90ae4974e03d66e7863e4cc88e6d87e6
ykominami <ykominami@gmail.com>
2023-10-06 14:37:13 +0900

    use ES-module

--- js/background.js
+++ js/background.js
@@ -1,9 +1,5 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript({code: "console.log('" + 'Turning ' + tab.url + ' red!' +"');"});
-/*
-    consoloe.log('Turning ' + tab.url + ' red!');
-    chrome.tabs.executeScript({
-	code: 'document.body.style.backgroundColor="red"'
-    });
-*/
+chrome.browserAction.onClicked.addListener(function (tab) {
+  chrome.tabs.executeScript({
+    code: "console.log('" + "Turning " + tab.url + " red!" + "');",
+  });
 });
