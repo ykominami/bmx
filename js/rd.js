8c7c1a7c2ef38b024e3ac5722808daf59cdb2b0e
ykominami <ykominami@gmail.com>
2024-04-01 01:24:11 +0900

    do not ignore js/debug.js
    use localstorage api. fix #2
    do not ignore js/debug.js
    add updateSelectRecently
    fix determine return value of funciton getItemByHier. fix #6
    from chrome-ext2/bmx2
    remove unused file
    double quoteをsingle quoteにする
    prettierの設定ファイル追加
    move outerjs

--- js/rd.js
+++ js/rd.js
@@ -1,12 +1,10 @@
-import { debugPrint2, debugPrint } from './debug.js';
+const array = ["Day", "2023", "202311", "20231125"]
 
-const array = ['Day', '2023', '202311', '20231125'];
-
-let result = array.reduce((accumulator, currentValue, currentIndex, array) => {
-  console.log(accumulator);
-  console.log(currentValue);
-  console.log('==');
-  return [accumulator, currentValue].join('/');
+let result = array.reduce( (accumulator, currentValue, currentIndex, array) => {
+	console.log(accumulator);
+	console.log(currentValue);
+	console.log("==");
+	return [accumulator, currentValue].join('/');
 });
 
 console.log(result);
