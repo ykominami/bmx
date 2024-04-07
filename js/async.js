60c8aa837ece6ce595b18ea52c724638d146437e
ykominami <ykominami@gmail.com>
2024-04-07 21:10:39 +0900

    use localstorage api. fix #2

--- js/async.js
+++ js/async.js
@@ -1,35 +1,8 @@
-import { getStorageOptions, adjustValue, addRecentlyItem } from './global.js';
-import { debugPrint2 } from './debug.js';
+import { getStorageOptions, addRecentlyItemX } from './global.js';
 
-function updateSelectRecently(select) {
-  console.log(`#- updateSelectRecently | async.js`);
-  addRecentlyItem(select, null, null);
+function restoreSelectRecently(select) {
+  let sOptions = getStorageOptions();
+  addRecentlyItemX(select, sOptions);
 }
 
-async function loadAsync_0() {
-  let ret = await loadSettings_by_api('from loadAsync');
-}
-// async function loadAsync(mes = "") {
-async function loadAsync() {
-  let ret = await loadSettings_by_api('from loadAsync');
-  let storageOptions = getStorageOptions();
-
-  console.log(
-    `loadSettings_by_api 0 --- Options=${JSON.stringify(
-      Object.entries(ret['Options'])
-    )}||`
-  );
-  storageOptions['Options'] = adjustValue(storageOptions['Options']);
-  console.log(
-    `loadSettings_by_api 1 --- Options=${JSON.stringify(
-      Object.entries(ret['Options'])
-    )}||`
-  );
-  console.log(`ret=${JSON.stringify(Object.entries(ret))}||`);
-
-  let options = [...ret['Options']];
-  console.log(`async.js | loadAsync options=${JSON.stringify(options)}||`);
-  updateSelectRecently($('#rinp'));
-}
-
-export { loadAsync, updateSelectRecently };
+export { restoreSelectRecently };
