079358a399760e48b09e18d5ef3c6ec32eb3be2d
ykominami <ykominami@gmail.com>
2024-04-08 08:28:30 +0900

    add updateSelectRecently

--- js/async.js
+++ js/async.js
@@ -9,4 +9,19 @@ function updateSelectRecently(select) {
   addRecentlyItem(select, null, null);
 }
 
-export { restoreSelectRecently };
+function updateSelectRecently(ary, select) {
+  // console.log(`updateSelectRecently ary=${JSON.stringify(ary)}`);
+  const opts1 = ary.map((element) => {
+    return $('<option>', {
+      value: element.value,
+      text: element.text,
+    });
+  });
+  select.empty();
+  select.append(opts1);
+  if (opts1.size > 0) {
+    select.val(ary[0].value);
+  }
+}
+
+export { restoreSelectRecently, updateSelectRecently };
