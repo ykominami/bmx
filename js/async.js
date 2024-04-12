c5145d65ae1774164e9c434c5b5e7a0ba7e5ae64
ykominami <ykominami@gmail.com>
2024-04-12 16:03:50 +0900

    from chrome-ext2/bmx2
    remove unused file
    double quoteをsingle quoteにする
    prettierの設定ファイル追加
    move outerjs

--- js/async.js
+++ js/async.js
@@ -1,29 +1,12 @@
-import { getStorageOptions, addRecentlyItemX } from './global.js';
+import { loadSettings, loadSettings_by_api } from './global.js';
 
-function restoreSelectRecently(select) {
-  let sOptions = getStorageOptions();
-  addRecentlyItemX(select, sOptions);
-}
-/*
- * function updateSelectRecently(select) {
-  console.log(`#- updateSelectRecently | async.js`);
-  addRecentlyItem(select, null, null);
-}
-*/
+function loadAsync(mes = '') {
+  return new Promise((resolve, reject) => {
+    // loadSettings(mes);
+    loadSettings_by_api(mes);
 
-function updateSelectRecently(ary, select) {
-  // console.log(`updateSelectRecently ary=${JSON.stringify(ary)}`);
-  const opts1 = ary.map((element) => {
-    return $('<option>', {
-      value: element.value,
-      text: element.text,
-    });
+    resolve({});
   });
-  select.empty();
-  select.append(opts1);
-  if (opts1.size > 0) {
-    select.val(ary[0].value);
-  }
 }
 
-export { restoreSelectRecently, updateSelectRecently };
+export { loadAsync };
