ea7e9a7737965d4bad720c40e005a7efc35f396e
ykominami <ykominami@gmail.com>
2025-09-06 14:56:48 +0900

    classify

--- js/data.js
+++ js/data.js
@@ -1,88 +1,83 @@
-let ItemHashByHier = {};
-let ItemHash = {};
+class Data {
+    #ItemHashByHier = {};
+    #ItemHash = {};
 
-function makeItemHashX(key) {
-    let hash = {};
-    hash[key] = {
-        ItemHashByHier: ItemHashByHier,
-        ItemHash: ItemHash,
-    };
-    return hash;
-}
+    constructor() {
+    }
 
-function dumpTreeItemsX(bookmarkTreeNodes) {
-    let ary = [];
-    let i;
-    for (i = 0; i < bookmarkTreeNodes.length; i++) {
-        const element = bookmarkTreeNodes[i];
-        if (element.url) {
-            ary.push(element.id);
+    makeItemHashX(key) {
+        let hash = {};
+        hash[key] = {
+            ItemHashByHier: this.#ItemHashByHier,
+            ItemHash: this.#ItemHash,
+        };
+        return hash;
+    }
+
+    dumpTreeItemsX(bookmarkTreeNodes) {
+        let ary = [];
+        let i;
+        for (i = 0; i < bookmarkTreeNodes.length; i++) {
+            const element = bookmarkTreeNodes[i];
+            if (element.url) {
+                ary.push(element.id);
+            }
+
+            if (element.children) {
+                ary = ary.concat(this.dumpTreeItemsX(element.children));
+            }
         }
+        return ary;
+    }
 
-        if (element.children) {
-            ary = ary.concat(dumpTreeItemsX(element.children));
+    dumpTreeItemsXTop(folder_id) {
+        let zary = [];
+        const item = this.getItem(folder_id);
+
+        chrome.bookmarks.getSubTree(item.id, (bookmarkTreeNodes) => {
+            zary = this.dumpTreeItemsX(bookmarkTreeNodes);
+        });
+        return zary;
+    }
+
+    getItemByHier(key) {
+        if (this.#ItemHashByHier[key]) {
+            return this.#ItemHashByHier[key];
+        } else {
+            return null;
         }
     }
-    return ary;
-}
 
-function dumpTreeItemsXTop(folder_id) {
-    let zary = [];
-    const item = getItem(folder_id);
+    setItemByHier(key, value) {
+        return (this.#ItemHashByHier[key] = value);
+    }
 
-    chrome.bookmarks.getSubTree(item.id, (bookmarkTreeNodes) => {
-        zary = dumpTreeItemsX(bookmarkTreeNodes);
-    });
-    return zary;
-}
+    getKeysOfItemByHier() {
+        return Object.keys(this.#ItemHashByHier);
+    }
 
-function getItemByHier(key) {
-    if (ItemHashByHier[key]) {
-        return ItemHashByHier[key];
-    } else {
-        return null;
+    getItemHashByHierKeys() {
+        return Object.keys(this.#ItemHashByHier);
+    }
+
+    getItem(key) {
+        if (this.#ItemHash[key]) {
+            return this.#ItemHash[key];
+        } else {
+            return null;
+        }
+    }
+
+    setItem(key, value) {
+        return (this.#ItemHash[key] = value);
+    }
+
+    addItem(item) {
+        // console.log(`addItem item=${JSON.stringify(item)}`);
+        this.setItem(item.id, item);
+        this.setItemByHier(item.hier, item);
     }
 }
 
-function setItemByHier(key, value) {
-    return (ItemHashByHier[key] = value);
-}
-
-function getKeysOfItemByHier() {
-    return Object.keys(ItemHashByHier);
-}
-
-function getItemHashByHierKeys() {
-    return Object.keys(ItemHashByHier);
-}
-
-function getItem(key) {
-    if (ItemHash[key]) {
-        return ItemHash[key];
-    } else {
-        return null;
-    }
-}
-
-function setItem(key, value) {
-    return (ItemHash[key] = value);
-}
-
-function addItem(item) {
-    // console.log(`addItem item=${JSON.stringify(item)}`);
-    setItem(item.id, item);
-    setItemByHier(item.hier, item);
-}
-
-export {
-    makeItemHashX,
-    dumpTreeItemsX,
-    dumpTreeItemsXTop,
-    getItemByHier,
-    setItemByHier,
-    getKeysOfItemByHier,
-    getItemHashByHierKeys,
-    getItem,
-    setItem,
-    addItem,
-};
+const data = new Data();
+export { data };
